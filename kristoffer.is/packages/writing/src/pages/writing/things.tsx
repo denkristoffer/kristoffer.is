bed946cb0b7c7f7f6f049084237cc86eb90148bd
denkristoffer <denkristoffer@users.noreply.github.com>
2020-05-03 20:57:38 +0200

    Set cache in render

--- kristoffer.is/packages/writing/src/pages/writing/things.tsx
+++ kristoffer.is/packages/writing/src/pages/writing/things.tsx
@@ -7,6 +7,9 @@ import { getAllPosts } from "../../lib/utils";
 import Link from "../../components/link";
 import { H2, Text } from "../../components/text";
 
+// Cache list of posts to get around page rendering without data
+const cache: { [key: string]: any[] } = {};
+
 const ArchiveH1 = H2.withComponent("h1");
 
 interface ArchivePost {
@@ -20,6 +23,10 @@ interface ArchiveProps {
 }
 
 export default function Archive({ posts }: ArchiveProps): React.ReactElement {
+  if (typeof window === "undefined") {
+    cache.posts = posts;
+  }
+
   return (
     <>
       <Head>
@@ -93,9 +100,6 @@ export default function Archive({ posts }: ArchiveProps): React.ReactElement {
     </>
   );
 }
-
-// Cache list of posts to get around page rendering without data
-const cache: { [key: string]: any[] } = {};
 
 export const getStaticProps: GetStaticProps = async () => {
   let posts;
