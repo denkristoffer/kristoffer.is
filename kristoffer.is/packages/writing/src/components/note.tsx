583fc9143146d403a47ba6b4cea826cebebb76b4
denkristoffer <denkristoffer@users.noreply.github.com>
2020-07-18 11:30:57 +0200

    feat: note

--- kristoffer.is/packages/writing/src/components/note.tsx
+++ kristoffer.is/packages/writing/src/components/note.tsx
@@ -1,7 +1,10 @@
 /** @jsx jsx */
 import { css, jsx } from "@emotion/react";
 
+import Aside from "./aside";
 import { Text } from "./text";
+
+import React from "react";
 
 interface NoteProps {
   children: React.ReactNode;
@@ -9,16 +12,19 @@ interface NoteProps {
 
 export default function Note({ children }: NoteProps): React.ReactElement {
   return (
-    <aside
+    <Aside
       css={(theme) => css`
-        background: #f3bb41;
-        border-radius: 3px;
-        margin: 0 auto 50px;
-        max-width: ${theme.sizes.maxWidth};
-        padding: 20px 0;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        background: ${theme.colors.warning};
+        color: #fff;
+
+        @media (prefers-color-scheme: dark) {
+          background: ${theme.dark.warning};
+        }
       `}
     >
       <Text>{children}</Text>
-    </aside>
+    </Aside>
   );
 }
